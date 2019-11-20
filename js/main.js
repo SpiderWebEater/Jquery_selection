672a2e9ebe68d41a2d964681c15a7ec2c06eefd3
Njteh <Njtehkk96@gmail.com>
2019-11-20 18:51:24 +0400

    New code

--- js/main.js
+++ js/main.js
@@ -1,61 +1,74 @@
 // picking elements from the DOM
-let tableContent = $("#table-content");
 let newInput = $("#new-input");
+let hamburger = $(".hamburger");
+let body = $("body");
+let ul = newInput.next();
+let tableContent = ul.next();
 let firstName = newInput.children().first().next();
 let lastName = firstName.next().next();
-let yearlyAverage= lastName.next().next();
-let firstSeAverage= yearlyAverage.next().next();
-let secondSeAverage= firstSeAverage.next().next();
-let errorMessage =  $("#error");
-let submit = $("#submit");
-let newStudent = $("#new-student");
-let ul = $("#ul-1");
-let burger = $(".hamburger");
-let nav = $(".nav-links");
-let disp = $(".display");
-let navLinks = $(".nav-links li");
+let yearlyAverage = lastName.next().next();
+let firstSeAverage = yearlyAverage.next().next();
+let secondSeAverage = firstSeAverage.next().next();
+let submit = secondSeAverage.next();
+let errorMessage = submit.next();
+let newStudent = newInput.prev().children(":nth-child(" + 2 + ")").children().children(":nth-child(" + 2 + ")").children();
+let wrap = newInput.prev().children(":nth-child(" + 2 + ")");
+
 //global variables
+let inputAreas = [firstName, lastName, yearlyAverage, firstSeAverage, secondSeAverage];
 let counter = 1;
 
 //function to execute after the document is loaded
-$(function(){
-    //click Event for NewStudent animation
-    newStudent.click(function () {
-        newInput.toggle(1000).css("display","flex");
+$(function () {
+    //hamburger menu
+    hamburger.on("click", function () {
+        wrap[0].classList.toggle("display-property");
+    });
+    newStudent.on("click", function () {
+        wrap[0].classList.toggle("display-property");
+    });
+
+    //this would allow the user to submit by pressing "Enter"
+    $("html").keydown(function (event) {
+        if (event.which === 13)
+            submitt();
     });
 
 
-   // all the functions to do after clicking submit
-   submit.click(function(){
-            let inputAreas =[firstName,lastName,yearlyAverage,firstSeAverage,secondSeAverage];
-        if(firstName.val() !==""&& lastName.val() !==""&&yearlyAverage.val()!==""&& firstSeAverage.val()!==""&& secondSeAverage.val()!==""){
+// function that changes focus from one input field to the next
+    for (let i = 0; i < inputAreas.length; i++) {
+        inputAreas[i].on("focus", function () {
+            $(this).on("keydown", function (event) {
+                if (event.keyCode === 38 || event.keyCode === 40)
+                    event.preventDefault();
+                if (i !== 4 && event.keyCode === 40)
+                    inputAreas[i + 1].focus();
 
-       let newElement = document.createElement("ul");
-       newElement.innerHTML = "<li>" + changeCapital(firstName.val()) +"</li><li>" + changeCapital(lastName.val())+"</li><li>"+parseInt(yearlyAverage.val())+"</li><li>"+
-           parseInt(firstSeAverage.val())+"</li><li>"+ parseInt(secondSeAverage.val())+"</li>";
-       newElement.classList.add("ul-"+(counter%2+1));
-       tableContent.append(newElement);
-       counter++;
-      errorMessage.css("display","none");
-       clearInput();
-            for (let i = 0; i <inputAreas.length ; i++) {
-                inputAreas[i].css("box-shadow","none");
-            }
 
-        }
-        else{
-            errorMessage.html("please fill in all the fields");
-            errorMessage.css("display","block");
-            for (let i = 0; i <inputAreas.length ; i++) {
-                if(inputAreas[i].val()==="")
-                    inputAreas[i].css("box-shadow", " 0 0 4px red");
-                else inputAreas[i].css("box-shadow", "0 0 4px green");
-            }
+                if (i !== 0 && event.keyCode === 38) {
+                    inputAreas[i - 1].focus();
+                }
 
-        }
-   });
+            })
+        })
+    }
 
-    let arayFunctionRef = ["first","last","yearly","firstSe","SecondSe"];
+
+    //click Event for NewStudent animation and display
+    newStudent.click(function () {
+        body[0].classList.toggle("display-property");
+        clearInput();
+
+    });
+
+
+    // all the functions to do after clicking submit
+    submit.on("click", function () {
+        submitt();
+    });
+
+
+    let arrayFunctionRef = ["first", "last", "yearly", "firstSe", "secondSe"];
     for (let i = 1; i <= 5; i++) {
         let child = ":first-child";
         if (i !== 1) {
@@ -64,32 +77,41 @@ $(function(){
         ul.children(child).click(function () {
             let arr = catching();
             tableContent.children().remove();
-            let arr2 = sortWith(arr,arayFunctionRef[i-1]);
+            let arr2 = sortWith(arr, arrayFunctionRef[i - 1]);
             recreate(arr2);
         });
     }
-    // click event for the hamburger menu
-
-    burger.addEventListener("click",()=>{
-        nav.parent.classList.toggle("toggle");
-    })
-
 
 });
 
-// functions
+// function that resets input field when submitt() is called properly
 function clearInput() {
-    firstName.val("");
-    lastName.val("");
-    yearlyAverage.val("");
-    firstSeAverage.val("");
-    secondSeAverage.val("");
+    errorMessage.css("display", "none");
+    for (let i = 0; i < inputAreas.length; i++) {
+        inputAreas[i].css("box-shadow", "none");
+        inputAreas[i].val("");
+    }
 }
 
-function changeCapital(name){
+//function to execute when submitt() is called and not all the input fields have values
+function dontClearInput() {
+    errorMessage.html("please fill in all the fields");
+    errorMessage.css("display", "block");
+    for (let i = 0; i < inputAreas.length; i++) {
+        if (inputAreas[i].val() === "")
+            inputAreas[i].css("box-shadow", " 0 0 4px red");
+        else inputAreas[i].css("box-shadow", "0 0 4px green");
+    }
+}
+
+
+//function that capitalizes the first letter of an inputted word
+function changeCapital(name) {
     return name.charAt(0).toUpperCase() + name.slice(1);
 }
 
+
+//function that stores the TableContents list items in an array of objects
 function catching() {
     let arr = [];
     for (let i = 0; i < tableContent.children().length; i++) {
@@ -108,6 +130,8 @@ function catching() {
     return arr;
 }
 
+
+//function that creates new tableContents list items with the proper sorting already done
 function recreate(arr) {
     for (let i = 0; i < arr.length; i++) {
         let newElement = document.createElement("ul");
@@ -117,10 +141,28 @@ function recreate(arr) {
     }
 }
 
-function sortWith(arr,key) {
-    arr.sort((a,b)=>(a[key]>=b[key])? 1:-1);
-    for (let i = 0; i < arr.length ; i++)
+//function that sorts the array input by given key and updates the index property of the i-th object of the array
+function sortWith(arr, key) {
+    arr.sort((a, b) => (a[key] >= b[key]) ? 1 : -1);
+    for (let i = 0; i < arr.length; i++)
         arr[i].index = i;
     return arr;
 
 }
+
+
+//function that makes a tableContent list item with the given elements
+function submitt() {
+    console.log("here");
+    if (firstName.val() !== "" && lastName.val() !== "" && yearlyAverage.val() !== "" && firstSeAverage.val() !== "" && secondSeAverage.val() !== "") {
+
+        let newElement = document.createElement("ul");
+        newElement.innerHTML = "<li>" + changeCapital(firstName.val()) + "</li><li>" + changeCapital(lastName.val()) + "</li><li>" + parseInt(yearlyAverage.val()) + "</li><li>" +
+            parseInt(firstSeAverage.val()) + "</li><li>" + parseInt(secondSeAverage.val()) + "</li>";
+        newElement.classList.add("ul-" + (counter % 2 + 1));
+        tableContent.append(newElement);
+        counter++;
+        clearInput();
+    } else dontClearInput();
+}
+
